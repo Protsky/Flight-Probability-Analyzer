c758daeb1c3d8a0ba3adc72e17e8e48312ccade3
Gionata <donati@gionata.ch>
2023-03-21 16:04:39 +0100

    add: graph

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ import json
 from collections import Counter, defaultdict
 from datetime import datetime
 from scipy.stats import poisson
+import matplotlib.pyplot as plt
 
 # Load data from JSON file
 with open('data.json', 'r') as f:
@@ -14,11 +15,24 @@ flights_by_month = defaultdict(Counter)
 for flight in data:
     date = datetime.strptime(flight['date'], '%d.%m.%Y')
     month = date.month
-    if flight['km'] > 200:
+    if flight['km'] > 700:
         flights_by_month[month]['flights'] += 1
 
 # Calculate Poisson probabilities for each month
-for month in range(1, 13):
+months = range(1, 13)
+probs = []
+for month in months:
     mu = flights_by_month[month]['flights']
     prob = 1 - sum([poisson.pmf(i, mu) for i in range(0, 6)])
-    print(f"Probability of having at least 6 flights of more than 700 km in month {month}: {prob*100:.2f}%")
+    probs.append(prob)
+
+# Create the bar chart
+fig, ax = plt.subplots()
+ax.bar(months, probs)
+ax.set_xlabel('Month')
+ax.set_ylabel('Probability')
+ax.set_ylim([0, 1])
+ax.set_xticks(months)
+ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+ax.set_title('Probability of having at least 6 flights of more than 700 km')
+plt.show()
